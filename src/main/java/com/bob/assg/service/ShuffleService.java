1f648bbcb464bbe8feee21434774d180f594d2a2
igal israilevich <iisrail@gmail.com>
2022-10-23 12:17:19 +0300

    Update ShuffleService.java

--- src/main/java/com/bob/assg/service/ShuffleService.java
+++ src/main/java/com/bob/assg/service/ShuffleService.java
@@ -3,15 +3,12 @@ package com.bob.assg.service;
 import java.util.Arrays;
 import java.util.Random;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import com.bob.assg.web.LogDto;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import reactor.core.publisher.Mono;
 
 @Slf4j
 @Service
@@ -19,13 +16,13 @@ import reactor.core.publisher.Mono;
 public class ShuffleService {
 
 	private final LogClient logClient;
-	
+
 	public int[] genereateArray(int size) {
-		LogDto logDto = new LogDto(size); 
+		LogDto logDto = new LogDto(size);
 		logClient.sendLog(logDto);
-		return genereateShuffledArray(size); 
+		return genereateShuffledArray(size);
 	}
-	
+
 	private int[] genereateShuffledArray(int size) {
 		log.debug("start genereateShuffledArray {}", size);
 		int[] arr = new int[size];
@@ -36,21 +33,19 @@ public class ShuffleService {
 		log.debug("end genereateShuffledArray array {} ", Arrays.toString(arr));
 		return arr;
 	}
-	
-    private void shuffle(int[] array) {
-    	Random random = new Random();
-        int count = array.length;
-        for (int i = count; i > 1; i--) {
-            swap(array, i - 1, random.nextInt(i));
-        }
-    }
 
-    private static void swap(int[] array, int i, int j) {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-	
-	
-	
+	private void shuffle(int[] array) {
+		Random random = new Random();
+		int count = array.length;
+		for (int i = count; i > 1; i--) {
+			swap(array, i - 1, random.nextInt(i));
+		}
+	}
+
+	private static void swap(int[] array, int i, int j) {
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+
 }
